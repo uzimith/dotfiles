315692e5cb5bcc2e2961390b251d8c4f9ae43dea
uzimith <uzimith.x9@gmail.com>
2024-01-07 01:10:04 +0900

    add

---
+++ .config/nvim/lua/plugins/rainbow_csv.lua
@@ -0,0 +1,19 @@
+return {
+  'mechatroner/rainbow_csv',
+  config = true,
+  ft = {
+    'csv',
+    'tsv',
+    'csv_semicolon',
+    'csv_whitespace',
+    'csv_pipe',
+    'rfc_csv',
+    'rfc_semicolon'
+  },
+  cmd = {
+    'RainbowDelim',
+    'RainbowDelimSimple',
+    'RainbowDelimQuoted',
+    'RainbowMultiDelim'
+  }
+}
