6b99150cdc6a2dec7fc39d6513b5bcb8b3048d61
uzimith <uzimith.x9@gmail.com>
2025-12-30 15:49:33 +0900

    fix

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -23,6 +23,7 @@ return {
       vim.keymap.set('i', '<Tab>', function()
         if require("copilot.suggestion").is_visible() then
           require("copilot.suggestion").accept()
+          return ''
         else
           return '<Tab>'
         end
