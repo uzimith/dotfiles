70d163a868de47107320cc9bdbf77b8e2396c543
uzimith <uzimith.x9@gmail.com>
2026-01-02 19:08:14 +0900

    fix

--- .config/nvim/lua/plugins/solarized.lua
+++ .config/nvim/lua/plugins/solarized.lua
@@ -11,9 +11,13 @@ return {
         variant = "spring",
         on_highlights = function(colors, color)
           return {
-            DiffAdd = { bg = color.lighten(colors.git_add, 70), fg = colors.git_add },
-            DiffChange = { bg = color.lighten(colors.git_modify, 70), fg = colors.git_modify },
-            DiffDelete = { bg = color.lighten(colors.git_delete, 70), fg = color.lighten(colors.git_delete, 70) },
+            DiffAdd = { fg = colors.git_add, bg = color.lighten(colors.git_add, 70) },
+            DiffChange = { fg = colors.git_modify, bg = color.lighten(colors.git_modify, 70) },
+            DiffDelete = {
+              fg = color.lighten(colors.git_delete, 70),
+              bg = color.lighten(colors.git_delete, 70),
+              reverse = true,
+            },
             DiffText = { fg = color.lighten(colors.cyan, 70), bg = colors.cyan, reverse = true },
           }
         end,
