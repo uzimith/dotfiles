b9a73ea5fa5d02ab0b4060651810cd975dee3216
uzimith <uzimith.x9@gmail.com>
2026-01-03 20:28:14 +0900

    add

--- .config/nvim/lua/plugins/make.lua
+++ .config/nvim/lua/plugins/make.lua
@@ -7,6 +7,32 @@ return {
       { "<leader>r", "<CMD>OverseerToggle<CR>" },
     },
     opts = {
+      actions = {
+        -- https://github.com/stevearc/overseer.nvim/blob/3cde0d84bdae56cd119cbf835f764fa30cec384c/lua/overseer/task_list/actions.lua#L175-L194
+        ["quickfix and close"] = {
+          desc = "open the entire task output in quickfix",
+          condition = function(task)
+            local bufnr = task:get_bufnr()
+            return task:is_complete()
+              and bufnr ~= nil
+              and vim.api.nvim_buf_is_valid(bufnr)
+              and vim.api.nvim_buf_is_loaded(bufnr)
+          end,
+          run = function(task)
+            local lines = vim.api.nvim_buf_get_lines(assert(task:get_bufnr()), 0, -1, true)
+            vim.fn.setqflist({}, " ", {
+              title = task.name,
+              lines = lines,
+              -- Peep into the default component params to fetch the errorformat
+              ---@diagnostic disable-next-line: invisible
+              efm = task.default_component_params.errorformat,
+            })
+            vim.cmd("botright copen")
+            -- add: close
+            vim.cmd("OverseerClose")
+          end,
+        },
+      },
       task_list = {
         direction = "right",
         keymaps = {
@@ -14,6 +40,7 @@ return {
           ["<C-w>"] = { "keymap.run_action", opts = { action = "watch" } },
           ["r"] = { "keymap.run_action", opts = { action = "restart" } },
           ["a"] = "keymap.run_action",
+          ["<C-q>"] = { "keymap.run_action", opts = { action = "quickfix and close" } },
         },
       },
       task_win = {
@@ -23,7 +50,8 @@ return {
       },
     },
     config = function(_, opts)
-      require("overseer").setup(opts)
+      local overseer = require("overseer")
+      overseer.setup(opts)
 
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "OverseerOutput",
@@ -31,6 +59,15 @@ return {
           vim.keymap.set("n", "q", "<CMD>quit<CR>", { buffer = true, nowait = true })
         end,
       })
+
+      -- pnpm run に --sequential を付ける（並列実行だと出力が混ざる）
+      overseer.add_template_hook({ module = "npm" }, function(task_defn)
+        vim.notify("Adding --sequential to pnpm run command", vim.log.levels.DEBUG)
+        local cmd = task_defn.cmd
+        if cmd and cmd[1] == "pnpm" and cmd[2] == "run" then
+          table.insert(cmd, 3, "--sequential")
+        end
+      end)
     end,
   },
   -- {
