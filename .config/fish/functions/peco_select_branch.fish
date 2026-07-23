64a0da0c01c17753183d5788c29e5a2ced9b5a57
uzimith <uzimith.x9@gmail.com>
2026-07-23 13:42:45 +0900

    fix

--- .config/fish/functions/peco_select_branch.fish
+++ .config/fish/functions/peco_select_branch.fish
@@ -1,4 +1,4 @@
 function peco_select_branch
     git branch --all --format='%(refname:short)' | peco $peco_flags | read branch
-    and wt switch -x 'herdr tab create --cwd {{ worktree_path }} --label {{ branch | sanitize }} --no-focus' $branch
+    and wt switch --no-cd -x 'herdr workspace create --cwd {{ worktree_path }} --label {{ branch | sanitize }} --focus' $branch
 end
